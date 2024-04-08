debfad4f3b92931a0b50169894d78621e275e817
Dawn Liu <dliu@lcs.on.ca>
2024-04-08 14:27:59 -0400

    Initial Commit

---
+++ Todo List/Todo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ListApp.swift
+//  Todo List
+//
+//  Created by Chenxi Liu on 2024/4/8.
+//
+
+import SwiftUI
+
+@main
+struct Todo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
